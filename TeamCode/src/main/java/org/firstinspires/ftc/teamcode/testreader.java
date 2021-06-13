f31abec2f9969aeb0749353ba9846fa324d23432
mvideet <mvideet@gmail.com>
2021-06-13 11:43:26 -0500

    Tested pushing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testreader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testreader.java
@@ -1,29 +1,29 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class testreader {
-    public static Scanner in;
-    {
-        try {
-            in = new Scanner(new File("inputs.in"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-   public static int N = in.nextInt();
-    public static Trajectory trajarr[] = new Trajectory[N+1];
-   public static void main(String[] args)
-   {
-       trajarr[0] =
-       for(int i =1;i<N+1;i++)
-       {
-           String s = in.next();
-       }
-   }
-
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.acmerobotics.roadrunner.trajectory.Trajectory;
+//
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.Scanner;
+//
+//public class testreader {
+//    public static Scanner in;
+//    {
+//        try {
+//            in = new Scanner(new File("inputs.in"));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//   public static int N = in.nextInt();
+//    public static Trajectory trajarr[] = new Trajectory[N+1];
+//   public static void main(String[] args)
+//   {
+//       trajarr[0] =
+//       for(int i =1;i<N+1;i++)
+//       {
+//           String s = in.next();
+//       }
+//   }
+//
+//}
