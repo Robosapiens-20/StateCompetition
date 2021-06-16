048de532c23e136fad042788c856266d72243bf2
Robosapiens-20 <69473959+Robosapiens-20@users.noreply.github.com>
2021-06-15 21:24:31 -0500

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Current_Auton/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Current_Auton/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static int shooterPower = 1475;
     public static int powerShotPower = 1350;
     public static double backShooterServo = 0.57;
-    public static double frontShooterServo = 0.4;
+    public static double frontShooterServo = 0.43;
     public static int oneRingThresholdRed = 130;
     public static int fourRingThresholdRed = 157;
     public static int oneRingThresholdBlue = 130;
